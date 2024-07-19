1f0ec12fd920d52e590f26a1d1e85a9d14229281
Egoriy <eg0by@yandex.ru>
2024-07-19 12:23:44 +0400

    add projects

--- script.js
+++ script.js
@@ -4,3 +4,49 @@ function toggleMenu() {
     nav.classList.toggle('show');
     burger.classList.toggle('close');
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let observer;
+
+    function initObserver() {
+        if (window.innerWidth < 1024) {
+            let options = {
+                root: document.querySelector('.projects-container'),
+                rootMargin: '0px',
+                threshold: 0.5
+            };
+
+            observer = new IntersectionObserver(function(entries, observer) {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('visible');
+                    } else {
+                        entry.target.classList.remove('visible');
+                    }
+                });
+            }, options);
+
+            document.querySelectorAll('.project').forEach(card => {
+                observer.observe(card);
+            });
+        } else {
+            // Удаляем все классы 'visible' если ширина экрана больше 1024px
+            document.querySelectorAll('.project').forEach(card => {
+                card.classList.remove('visible');
+            });
+        }
+    }
+
+    // Инициализация наблюдателя при загрузке страницы
+    initObserver();
+
+    // Повторная инициализация наблюдателя при изменении размеров окна
+    window.addEventListener('resize', function() {
+        // Удаляем все наблюдения при изменении размера, если observer определен
+        if (observer) {
+            observer.disconnect();
+        }
+        // Повторная инициализация
+        initObserver();
+    });
+});
